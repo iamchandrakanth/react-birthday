6869a77cb5041c735ac5880448a9ba515bbef8fb
iamchandrakanth <ccmchandrakanth5@gmail.com>
2021-12-09 17:52:24 +0530

    birthday

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: 1,
+        name: 'Ramesh',
+        age: 26,
+        image: 'https://www.tiss.edu/uploads/images/Webp.net-compress-image_1.jpg'
+    },
+    {
+        id: 2,
+        name: 'Suresh',
+        age: 42,
+        image: 'https://st1.bollywoodlife.com/wp-content/uploads/2021/09/Ramesh-Valiyasala.jpg'
+    },
+    {
+        id: 3,
+        name: 'Naresh',
+        age: 54,
+        image: 'https://pbs.twimg.com/profile_images/1236534548424314881/XLTge1_h_400x400.jpg'
+    },
+    {
+        id: 4,
+        name: 'Ganesh',
+        age: 21,
+        image: 'https://dz01iyojmxk8t.cloudfront.net/wp-content/uploads/2019/08/28065349/IMG_20190724_153816_019.jpg'
+    },
+    {
+        id: 5,
+        name: 'Mukhesh',
+        age: 47,
+        image: 'https://nettv4u.com/imagine/12-12-2018/bijili-ramesh.jpg'
+    }
+
+]
